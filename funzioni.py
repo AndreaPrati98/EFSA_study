8e7d0c19d67a5f1da41d2db56eac60567a076bc4
AndreaPrati98 <62189267+AndreaPrati98@users.noreply.github.com>
2020-11-28 16:36:28 +0100

    aggiunta funzione per la backward. Problema: non fa il modello con 3 predittori

--- funzioni.py
+++ funzioni.py
@@ -7,6 +7,7 @@ import seaborn as sns
 import xlrd
 import time
 import statsmodels.api as sm
+import itertools
 
 def CarloCrecco():
     print("Cuindknpjbjfenjja!")
@@ -49,6 +50,32 @@ def forward(y, X, predictors, weights, yesPrint):
     # Return the best model, along with some other useful information about the model
     return best_model
 
+def backward(Y, X, predictors, weights, yesPrint):
+    
+    tic = time.time()
+    
+    results = []
+
+    # We do so, because the next for-cycle will not compute the model with the max
+    # results.append(fn.processSubset(Y, X, pred, weights))   
+    
+    for combo in itertools.combinations(predictors, len(predictors)-1):
+        results.append(processSubset(Y, X, combo, weights))
+    
+    # Wrap everything up in a nice dataframe
+    models = pd.DataFrame(results)
+    if yesPrint:
+        display(models)
+    
+    # Choose the model with the highest RSS
+    best_model = models.loc[models['RSS'].argmin()]
+    
+    toc = time.time()
+    print("Processed ", models.shape[0], "models on", len(predictors)-1, "predictors in", (toc-tic), "seconds.")
+    
+    # Return the best model, along with some other useful information about the model
+    return best_model
+
 # Funzione per calcolare 
 def mainForward(X, Y, weights, yesPrint = False):
     models_fwd = pd.DataFrame(columns=["RSS", "model", "number_of_predictors", "name_of_predictors","Y_pred"])
@@ -63,6 +90,21 @@ def mainForward(X, Y, weights, yesPrint = False):
     toc = time.time()
     print("Total elapsed time:", (toc-tic), "seconds.")
     return models_fwd
+
+def mainBackward(X, Y, weights, yesPrint = False):
+    models_bwd = pd.DataFrame(columns=["RSS", "model", "number_of_predictors", "name_of_predictors","Y_pred"])
+
+    tic = time.time()
+    predictors = X.columns
+    
+    while(len(predictors) > 1):
+        offset = len(predictors)
+        models_bwd.loc[offset] = backward(Y, X, predictors, weights, yesPrint)
+        predictors = models_bwd.loc[offset]["model"].model.exog_names
+
+    toc = time.time()
+    print("Total elapsed time:", (toc-tic), "seconds.")
+    return models_bwd
 
 def compute_criteria(group_of_models):
     for i in range(1, group_of_models.shape[0]+1):
